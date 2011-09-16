c1672938265d8d7221776c6965d81aa257c9e9ab
rajido <rajido@57973d08-368f-811e-92e5-968bbe8de791>
2011-09-16 13:58:43 +0000

    add miscore-example

---
+++ trunk/miscore-example/src/main/java/MIScoreClient.java
@@ -0,0 +1,64 @@
+import uk.ac.ebi.enfin.mi.score.scores.MIScore;
+
+import java.util.ArrayList;
+
+/**
+ * This example show how to use MIscore to obtain confidence scores for
+ * molecular interactions using standard information easy to find in any database
+ * adopting the PSI-MITAB format:
+ *   - Experimental detection methods found for the interaction
+ *   - Number of publications
+ *   - Interaction types found for the interaction
+ *
+ * More information in:
+ *   - http://code.google.com/p/miscore/
+ *
+ * User: rafael
+ * Date: 16-Sep-2011
+ * Time: 09:43:20
+ */
+public class MIScoreClient {
+      public static void main(String [] args){
+        /* METHOD ONTOLOGY TERMS */
+          /** If the same method has been reported for one interaction more than once
+           * in the same publication, then the method should be included in MIscore just once.
+           * If the same method has been reported for one interaction more than once
+           * in different publication, then the method should be included in MIscore as many times
+           *  as it appears in different publications. */
+        ArrayList methodInput = new ArrayList();
+        methodInput.add("MI:0051");
+        methodInput.add("MI:0051");
+        methodInput.add("MI:0042");
+
+        /* TYPE ONTOLOGY TERMS */
+          /** If the same type has been reported for one interaction more than once
+           * in the same publication, then the type should be included in MIscore just once.
+           * If the same type has been reported for one interaction more than once
+           *  in different publication, then the type should be included in MIscore as many times
+           *  as it appears in different publications. */
+        ArrayList typeInput = new ArrayList();
+        typeInput.add("MI:0208");
+        typeInput.add("MI:0403");
+        typeInput.add("MI:0407");
+
+        /* NUMBER OF PUBMED PUBLICATION */
+        Integer publicationInput = 4;
+
+        /* SET CONFIDENCE SCORE */
+        MIScore mis = new MIScore();
+        mis.setMethodScore(methodInput);
+        mis.setTypeScore(typeInput);
+        mis.setPublicationScore(publicationInput);
+
+        /* PRINT SCORES */
+          /*  - Molecular interaction confidence score */
+          System.out.println("- Molecular interaction confidence score: " + mis.getScore());
+          /*  - Method score */
+          System.out.println("  * Method score: " + mis.getMethodScore());
+          /*  - Type score */
+          System.out.println("  * Type score: " + mis.getTypeScore());
+          /*  - Publication score */
+          System.out.println("  * Publication score: " + mis.getPublicationScore());
+
+      }
+}
